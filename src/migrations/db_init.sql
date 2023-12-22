e1f1553527afe1c286e9b1934b4c9d4ea2a43c40
AbiyuTolosa <abiyunigussie7@gmail.com>
2023-12-22 21:08:52 +0300

    admin table query added.

--- src/migrations/db_init.sql
+++ src/migrations/db_init.sql
@@ -63,3 +63,10 @@ CREATE TABLE OrderDetails (
     FOREIGN KEY (OrderID) REFERENCES Orders(OrderID),
     FOREIGN KEY (BookID) REFERENCES Books(BookID)
 );
+
+CREATE TABLE admin (
+    Admin_id INT AUTO_INCREMENT PRIMARY KEY,
+    Username VARCHAR(50) NOT NULL,
+    Password VARCHAR(255) NOT NULL,
+    Email VARCHAR(100) NOT NULL
+);
